32bb0a64b70ac0395f22e1e1bd4faf4f9a92438e
kusumahara <vamsiviswanath7@gmail.com>
2024-07-20 01:04:45 +0530

    Update UITest.java

--- mavenforjenkins/src/test/java/mavenforjenkins/UITest.java
+++ mavenforjenkins/src/test/java/mavenforjenkins/UITest.java
@@ -22,7 +22,7 @@ public class UITest
 	driver = new ChromeDriver();
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/logi");
 	
 	}}
 //vamsi changed.
