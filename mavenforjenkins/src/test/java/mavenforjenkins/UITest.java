ec0a7759e03e4b3d3d07d90b45a8cd9943c32cd4
sss <sss@anukya>
2024-07-20 00:52:18 +0530

    FIRIST COMMIT

---
+++ mavenforjenkins/src/test/java/mavenforjenkins/UITest.java
@@ -0,0 +1,28 @@
+package mavenforjenkins;
+
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class UITest 
+{
+
+	WebDriver driver;
+	@Test
+	public void startBrowser() {
+
+	driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	
+	}}
+//vamsi changed.
